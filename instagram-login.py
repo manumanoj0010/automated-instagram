3f6027b1a60f186c31e5bcda69623e0e74bf4637
manumanoj0010 <manumanoj0010@gmail.com>
2019-05-01 20:26:00 +0530

    updating the first file

---
+++ instagram-login.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from time import sleep
+
+
+class App:
+    def __init__(self,username='m.a.n.u.m.a.n.o.j', password='321Password.com', target_username='m.a.n.u.m.a.n.o.j', path='C:/personal/photos'):
+        self.username=username
+        self.password=password
+        self.target_username=target_username
+        self.path=path
+        self.driver=webdriver.Chrome('C:\Additional Softwares\chromedriver.exe')
+        self.main_url='https://www.instagram.com'
+        self.driver.get(self.main_url)
+        sleep(3)
+
+        self.log_in()
+
+        sleep(3)
+        self.driver.close()
+
+    def log_in(self,):
+        log_in_button=self.driver.find_element_by_xpath('//div[@class="gr27e"]/p[@class="izU2O"]/a')
+        log_in_button.click()
+        sleep(3)
+        user_name_input=self.driver.find_element_by_xpath('//input[@name="username"]')
+        user_name_input.send_keys(self.username)
+        password_input=self.driver.find_element_by_xpath('//input[@name="password"]')
+        password_input.send_keys(self.password)
+        password_input.submit()
+        input('submit for now')
+
+
+if __name__=='__main__':
+    app=App()
